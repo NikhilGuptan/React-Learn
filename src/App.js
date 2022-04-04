eb6a85c0cf07b6a1422f08639514c78f155fe4aa
Nikhil4.Gupta <ng3152340@gmail.com>
2022-04-04 11:14:07 +0530

    Stritng the react learn

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
+import { Event } from './Components/Event';
+import { Props } from './Components/Props';
+import { ConditionalRendring } from './Components/ConditionalRendring';
+import { List } from './Components/List';
+import { Todo } from './Components/Todo';
 
 function App() {
+  const [counter,setCounter] = useState(0)
+
+  const handfleCounter= () =>{
+    setCounter(counter+1)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Event counter={counter}/>
+      <Props handfleCounter = {handfleCounter}/> */}
+      {/* <ConditionalRendring/> */}
+      {/* <List/> */}
+      <Todo/>
     </div>
   );
 }
